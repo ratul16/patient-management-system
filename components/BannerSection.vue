5ba4690d4e8f4f8c94acefd102ed44c01643e404
ratul16 <haratul@gmail.com>
2025-06-22 01:22:37 +0100

    feat: enhance BannerSection header styling for improved visibility

--- components/BannerSection.vue
+++ components/BannerSection.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="element-bg rounded-lg p-6 flex items-center justify-between">
     <div>
-      <h4 class="mb-2">Welcome {{ name }}</h4>
+      <h4 class="mb-2 text-2xl element-header font-bold">Welcome {{ name }}</h4>
       <p>
         Ensure your well-being by letting us assess your health. Take proactive steps towards a
         healthier life starting today for a brighter future.
